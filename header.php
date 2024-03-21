877ffad8995160668cfe2ef5679cf61526484e62
Team53FR <augrosrobin@gmail.com>
2024-03-21 18:31:12 +0100

    Correction CSS

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <header>
-    <h1>Chaîne Twitch de [Nom de la chaîne]</h1>
+    <h1>Bienvenue au GangDesGueux !</h1>
     <nav>
         <ul>
+            <li><a href="index.php?page=accueil">Accueil</a></li>
+            <li><a href="index.php?page=sponsor">Sponsor</a></li>
             <li><a href="index.php?page=about">À propos</a></li>
-            <li><a href="index.php?page=schedule">Horaire</a></li>
-            <li><a href="index.php?page=videos">Vidéos</a></li>
             <li><a href="index.php?page=contact">Contact</a></li>
         </ul>
     </nav>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Chaîne Twitch - Accueil</title>
+<title>GangDesGueux</title>
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -28,7 +28,7 @@
     } 
     else 
     {
-        include 'accueil.php'; // Page par défaut
+        include 'Vue/accueil.php'; // Page par défaut
     }
     ?>
 </main>
